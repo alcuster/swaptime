2cbdf99886fd5a63183bcda669ef071e65beaa3b
Alec Custer <aleccuster@alecs-air.attlocal.net>
2017-01-12 12:21:16 -0600

    fix UI and course model

--- both/models/course.model.ts
+++ both/models/course.model.ts
@@ -8,15 +8,6 @@ export interface Course extends CollectionObject {
   title: string;
   days: string;
   time: string;
-  cap: number;
-  act: number;
-  rem: number;
-  wl_cap: number;
-  wl_act: number;
-  wl_rem: number;
-  xl_cap: number;
-  xl_act: number;
-  xl_rem: number;
   instructor: string;
   location: string;
   distreq: string;
--- client/imports/app/submit/times.model.ts
+++ client/imports/app/submit/times.model.ts
@@ -12,10 +12,14 @@ export class Times {
         this.times.push({label: '9:30-10:20', value: '09:30 am-10:20 am'});
         this.times.push({label: '9:40-10:55', value: '09:40 am-10:55 am'});
         this.times.push({label: '10:30-11:20', value: '10:30 am-11:20 am'});
-        this.times.push({label: '11:30-12:20', value: '11:30 am-12:20 am'});
-        this.times.push({label: '12:15-1:30', value: '12:15 am-1:30 am'});
-        this.times.push({label: '12:30-1:20', value: '12:30 am-12:20 am'});
-        this.times.push({label: '1:40-2:55', value: '01:40 am-2:55 am'});
+        this.times.push({label: '11:30-12:20', value: '11:30 am-12:20 pm'});
+        this.times.push({label: '12:15-1:30', value: '12:15 pm-01:30 pm'});
+        this.times.push({label: '12:15-4:20', value: '12:15 pm-04:20 pm'});
+        this.times.push({label: '12:30-1:20', value: '12:30 pm-12:20 pm'});
+        this.times.push({label: '1:30-2:20', value: '01:30 pm-02:20 pm'});
+        this.times.push({label: '1:30-4:20', value: '01:30 pm-04:20 pm'});
+        this.times.push({label: '1:40-2:55', value: '01:40 pm-02:55 pm'});
+
     }
 
 }
