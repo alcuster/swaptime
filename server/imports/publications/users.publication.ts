25843639f4fe86c5787878f081efd0f54f78c767
Alec Custer <acuster25@gmail.com>
2017-04-20 17:12:23 -0400

    change Users publication to hide display name, avatar, and profile picture from users who are not logged in or do not have a verified email

--- server/imports/publications/users.publication.ts
+++ server/imports/publications/users.publication.ts
@@ -5,11 +5,18 @@ import { Users } from '../../../both/collections/users.collection';
 import { User } from '../../../both/models/user.model';
 
 Meteor.publish('users', function(): Mongo.Cursor<User> {
-  //if (!this.userId) return;
+  if (!this.userId) {
+    return Users.collection.find({}, { fields: { 'profile.avatar': 0, 'profile.picture': 0, 'profile.displayname': 0 } });
+  }
 
-  return Users.collection.find({}, {
-    fields: {
-      profile: 1
-    }
-  });
+  const unverified = Meteor.users.findOne({_id: this.userId, 'emails.0.verified': false});
+
+  if (unverified) {
+    return Users.collection.find({}, { fields: { 'profile.avatar': 0, 'profile.picture': 0, 'profile.displayname': 0 } });
+  }
+
+  //Iff a user is logged and in has a verified email, show other users'
+  //entire profiles
+  return Users.collection.find({}, { fields: { profile: 1 } });
+
 });
